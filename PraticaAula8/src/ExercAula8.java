a4ae34875d5d07c5720ffcabe26f4440ae407513
aly <alymacome@gmail.com>
2019-12-15 22:07:23 +0200

    Resolucao dos exercicios da aula8

---
+++ PraticaAula8/src/ExercAula8.java
@@ -0,0 +1,118 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aly Macome
+ */
+public class ExercAula8 {
+    
+     public static void main(String[] args) {
+        
+       ArrayList<String> Lista = new ArrayList<>();
+        ArrayList<String> al = new ArrayList<>();
+        Vector v = new Vector(5, 5);
+        
+        // a)
+        v.add(3);
+        
+        // b)
+        al.add("31 anos");
+        
+        // c)
+        al.add("10");
+             
+        // d)        
+        al.clear();
+        
+        // e)
+        ArrayList<String> ss = new ArrayList<>();
+        
+        // f)
+        ss.add("Teste");
+        
+        // g)
+        Lista.add("PAULO");
+        
+        //h)
+        Vector<Double> dar = new Vector <>();
+        
+        // i)
+        dar.add(2.0);
+        
+        // j)
+        dar = new Vector<>();
+             
+                
+        // k)
+        dar.clear();
+        
+        // l)
+        v.add(4);
+        
+        // m)
+        ss.addAll(v);
+        
+        // n)
+        ss.isEmpty();
+             
+        // o)
+        al.remove("31 anos");
+        
+        // p)
+       
+        al.indexOf(v);
+        
+        // q)
+        al.size();
+        
+        //r)
+        //lista.removeAll()
+        Lista.removeAll(v);
+        
+        
+        //s)
+        //array.pop()
+        array.pop()
+        
+        
+        // t)
+        //meuArrar.remove;
+        Lista.remove("");
+        
+        // u)
+        Arrays.asList(al);
+        
+        // v)
+        Arrays.toString(ss.toArray());
+        
+        // w)
+        ArrayList<String> nome = new ArrayList();
+        
+        // x)
+        Vector<Double> LKC = new Vector<>();
+        
+        // y)
+         LKC.pop();
+        
+        
+        // y)
+        //arrar.clear;
+        ss.clear();
+        
+        
+        // z)
+        ss.remove("");
+        
+    }
+    
+    
+}
