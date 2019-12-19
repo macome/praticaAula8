3819c548d471f0759df528e30336ac9b8c093883
aly <alymacome@gmail.com>
2019-12-19 18:48:12 +0200

    conversao minutos para segundos

--- PraticaAula8/src/PooAula8/ConversaoDaUnidadeTempo.java
+++ PraticaAula8/src/PooAula8/ConversaoDaUnidadeTempo.java
@@ -9,6 +9,20 @@ package PooAula8;
  *
  * @author Aly Macome
  */
+/*
+2.	Escreva a classe ConversaoDeUnidadesDeTempo com métodos estáticos para conversão
+        aproximada das unidades de tempo segundo a lista abaixo. 
+•	1 minuto = 60 segundos 
+•	1 hora = 60 minutos 
+•	1 dia = 24 horas
+•	1 semana = 7 dias 
+•	1 mês = 30 dias 
+•	1 ano = 365.25 dias
+
+*/
 public class ConversaoDaUnidadeTempo {
+    public static double minuSegundo(double min){
+        return min * 60;
+    }
     
 }
